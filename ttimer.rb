30f30d86fdd3ba191ddd22b09c4aee46a8b9535d
goreleaserbot <goreleaser@carlosbecker.com>
2022-03-20 19:58:29 -0700

    Brew formula update for ttimer version v1.0.11

--- ttimer.rb
+++ ttimer.rb
@@ -2,16 +2,16 @@
 class Ttimer < Formula
   desc ""
   homepage "https://github.com/drgrib/ttimer"
-  version "1.0.10"
+  version "1.0.11"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/drgrib/ttimer/releases/download/v1.0.10/ttimer_1.0.10_darwin_amd64.tar.gz"
-    sha256 "e224d74d5a81bebd8251148d7f47917d7c34da2522f5f3ab21e4841183907a1b"
+    url "https://github.com/drgrib/ttimer/releases/download/v1.0.11/ttimer_1.0.11_darwin_amd64.tar.gz"
+    sha256 "2ab1eaf140ee06fa346beec235c9ddb6649ff25a9c51631672453b57abc158ba"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/drgrib/ttimer/releases/download/v1.0.10/ttimer_1.0.10_linux_amd64.tar.gz"
-      sha256 "4fd7daeed636b16f7d448c48fc7dd7306797cf38f55050508ccd9ba18f2e3a28"
+      url "https://github.com/drgrib/ttimer/releases/download/v1.0.11/ttimer_1.0.11_linux_amd64.tar.gz"
+      sha256 "3a48fed49f05d43ba178aa2f7c9ad7651ae7b7fe377b2fe487b55f22f0868ebb"
     end
   end
 
